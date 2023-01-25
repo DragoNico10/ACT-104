53256cd400aecd1438b5ca14f7068a711db335e1
DragoNico10 <82358124+DragoNico10@users.noreply.github.com>
2023-01-25 15:52:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import tkinter.messagebox
+#Change font here
 font=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
 img=cv2.imread('solar-system.jpg')
 cv2.putText(img, 'Sun', (100,80), font, 2, (27,111,237))
@@ -12,7 +13,7 @@ cv2.putText(img, 'Jupiter', (480, 50), font, 2, (88, 132, 191))
 cv2.putText(img, 'Saturn', (740,320), font, 1.5, (143, 167, 202))
 cv2.putText(img, 'Uranus', (950, 300), font, 1, (236, 233, 195))
 cv2.putText(img, 'Neptune', (1090, 300), font, 1, (220, 86, 58))
-cv2.imshow('',img)
+cv2.imshow('ACT-104',img)
 
 if tkinter.messagebox.askyesno('ACT-104','Save image?'):
     cv2.imwrite('solar-system(with text).jpg', img)
